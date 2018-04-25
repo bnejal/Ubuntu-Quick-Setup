3fc6310d80e13b5428406393fa721613fcc9319a
Brandon <bnejal@gmail.com>
2018-04-24 20:21:37 -0700

    script improvements
    changed how snaps are installed on this script

--- setup_for_clean_install.sh
+++ setup_for_clean_install.sh
@@ -3,9 +3,14 @@
 sudo add-apt-repository ppa:linrunner/tlp
 sudo add-apt-repository multiverse
 sudo apt-get update && sudo apt-get upgrade 
-sudo apt-get install xorg xauth ubuntu-gnome-desktop gnome-shell firefox gnome-tweak-tool tlp tlp-rdw gufw snapd software-properties-gtk steam hardinfo wine
+sudo apt-get install xorg xauth ubuntu-gnome-desktop gnome-shell firefox gnome-tweak-tool tlp tlp-rdw gufw snapd software-properties-gtk steam hardinfo wine-stable
 sudo apt install libqt5svg5 qtdeclarative5-qtquick2-plugin qtdeclarative5-window-plugin qtdeclarative5-controls-plugin
-sudo snap install spotify bluez google-play-music-desktop-player vscode simplenote gitkraken
+sudo snap install spotify 
+sudo snap install bluez 
+sudo snap install google-play-music-desktop-player 
+sudo snap install --classic vscode 
+sudo snap install simplenote 
+sudo snap install gitkraken
 
 echo "this script has completed installation for the following:"
 echo "xorg"
